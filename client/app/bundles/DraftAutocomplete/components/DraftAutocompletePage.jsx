d58f90e439c929338a4968c76115ceeefb1c62a1
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2017-01-15 10:28:52 -0800

    Install React

---
+++ client/app/bundles/DraftAutocomplete/components/DraftAutocompletePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DraftAutocompletePage = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-12">
+        <h1>Draft Autocomplete</h1>
+      </div>
+    </div>
+  </div>
+)
+
+export default DraftAutocompletePage
---
+++ client/app/bundles/DraftAutocomplete/startup/DraftAutocompleteApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import DraftAutocompletePage from '../components/DraftAutocompletePage'
+
+
+export default function DraftAutocompleteApp(props, railsContext) {
+  return (
+    <DraftAutocompletePage />
+  )
+}
